04d9294df6fdcaa93313d4140bdd82e73d5dbb43
Andrew Fernandes <48365833+andrew8gmf@users.noreply.github.com>
2024-10-07 14:17:26 -0300

    chore: update codebase to comply with linting rules

--- src/zone-flags.ts
+++ src/zone-flags.ts
@@ -3,4 +3,7 @@
  * running with certain Web Component callbacks
  */
 // eslint-disable-next-line no-underscore-dangle
-(window as any).__Zone_disable_customElements = true;
+interface Window {
+  __Zone_disable_customElements?: boolean;
+}
+(window as Window).__Zone_disable_customElements = true;
